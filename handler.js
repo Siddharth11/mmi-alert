4a7a2dae96d9a69be323ff3a5f0ed62ae5d46e09
Siddharth Parmar <siddharth.parmar@zalando.de>
2019-01-30 21:38:34 +0100

    Add axios

--- handler.js
+++ handler.js
@@ -1,11 +1,16 @@
 'use strict';
 
-module.exports.mmiAlert = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const axios = require('axios');
+
+module.exports.mmiAlert = async () => {
+  const url = 'https://api.smallcase.com/market/indices/marketMoodIndex/current';
+
+  try {
+    const response = await axios.get(url);
+    const { currentValue } = response.data.data;
+    console.log(currentValue);
+  } catch(error) {
+    console.log('An error occurred:');
+    console.log(error);
+  }
 };
